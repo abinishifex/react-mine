4c9a72e508f8dd6e6b80d5375692ab708c04f832
abinishifex <abinishifex@gmail.com>
2025-12-02 21:33:02 -0900

    dream project

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,28 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <h1>Welcome to My Dream Project</h1>
+      <h2>Your one-stop place for productivity</h2>
+
+      <p>
+        This is the landing page of my new project. I will build it step-by-step
+        as I learn React.
+      </p>
+
+      <h3>What this project will include:</h3>
+      <ul>
+        <li>Simple and clean user interface</li>
+        <li>Useful daily tools</li>
+        <li>Fun and easy-to-use features</li>
+      </ul>
+
+      <p>Stay tuned for more updates!</p>
+    </div>
   );
 }
 
 export default App;
+
+
+
