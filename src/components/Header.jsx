4eac5891fa479f2027531c6359e7880d909c8117
abinishifex <abinishifex@gmail.com>
2025-12-02 21:02:50 -0900

    inital commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header>header22</header>;
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Home(){
+  return(
+    <div>
+      <h1>welcome to my TO-DO-APP</h1>
+    </div>
+  );
+}
+export default Home;
